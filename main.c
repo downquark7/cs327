3ac01170032b57819cf24bdf43b9936d0efb82aa
enicely <mail2emn@gmail.com>
2023-01-19 12:09:01 -0600

    added some functions to encode data efficiently

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+#define width 5
+#define height 5
+
+/***
+ * @param bit 0-(length-1) bit of number
+ * @param num number representing a board state or list of positions to get bit from
+ * @return 1 or 0
+ */
+int getBit(int bit, int num) {
+    return (num >> bit) & 1;
+}
+
+int flipBit(int bit, int num) {
+    return num ^ (1 << bit);
+}
+
+int fullBoard = (1 << (width * height)) - 1;
+
+int isBoardFull(int board) {
+    return board == fullBoard;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    printf("%d", 2147483647);
     return 0;
 }
