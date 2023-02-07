c383eca9dfa00c58ec9e0f17e1a545367e051547
enicely <mail2emn@gmail.com>
2023-02-07 16:30:10 -0600

    update submit script

--- submit.sh
+++ submit.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/sh
 git --no-pager log > CHANGELOG
 make clean
-rm -rf nicely_evan.assignment-1.02/
 mkdir nicely_evan.assignment-1.02
 cp Makefile nicely_evan.assignment-1.02/
 cp README nicely_evan.assignment-1.02/
@@ -9,3 +8,4 @@ cp CHANGELOG nicely_evan.assignment-1.02/
 cp main.c nicely_evan.assignment-1.02/
 cp -r map/ nicely_evan.assignment-1.02/map/
 tar -czvf nicely_evan.assignment-1.02.tar.gz nicely_evan.assignment-1.02
+rm -rf nicely_evan.assignment-1.02/
