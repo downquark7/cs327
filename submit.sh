e11965ebe420a8d394f244d585a5c2b418a7f3ee
enicely <mail2emn@gmail.com>
2023-02-16 12:15:43 -0600

    update submit script and clean files

--- submit.sh
+++ submit.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/sh
 rm -rf nicely_evan.assignment-1.03/
-git --no-pager log >CHANGELOG
-make clean
 mkdir nicely_evan.assignment-1.03
-cmake CMakeLists.txt
 cp -rfv * nicely_evan.assignment-1.03/
-rm -rfv nicely_evan.assignment-1.03/cmake-build-debug/
-rm -rfv nicely_evan.assignment-1.03/knightsTour/
-rm -rfv nicely_evan.assignment-1.03/Testing/
-rm -rfv nicely_evan.assignment-1.03/.*
-rm -rfv nicely_evan.assignment-1.03/nicely_evan.assignment-1.03/
-rm -rfv nicely_evan.assignment-1.03/*.tar.gz
-cd nicely_evan.assignment-1.03
+cd nicely_evan.assignment-1.03/
+git --no-pager log >CHANGELOG
+cmake CMakeLists.txt
+rm -rfv cmake-build-debug/
+rm -rfv knightsTour/
+rm -rfv Testing/
+rm -rfv .*
+rm -rfv
+rm -rfv *.tar.gz
 make
 ./main
 make clean
