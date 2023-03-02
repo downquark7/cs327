c5acda652dda29271d523b2ec8e0e889cba0c767
enicely <mail2emn@gmail.com>
2023-03-02 00:03:00 -0600

    update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 rm -rf nicely_evan.assignment-1.04/
+rm -rf nicely_evan.assignment-1.04.tar.gz
 rm -rf /tmp/nicely_evan.assignment-1.04/
 mkdir nicely_evan.assignment-1.04
 cp -rfv * nicely_evan.assignment-1.04/
